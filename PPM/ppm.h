738e27264769a72d0dfd09805b5ce88dbfc0f36d
Ada-Armstrong <christopherada.a@gmail.com>
2019-12-06 17:10:09 -0500

    ppm now saves as binary

--- PPM/ppm.h
+++ PPM/ppm.h
@@ -1,10 +1,11 @@
 #ifndef __PPM_H__
 #define __PPM_H__
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
 
-struct rgb {
+struct rgb_ppm {
 	uint8_t red;
 	uint8_t green;
 	uint8_t blue;
@@ -14,14 +15,13 @@ struct ppm {
 	unsigned int width;
 	unsigned int height;
 	uint8_t depth;
-	struct rgb *data;
+	struct rgb_ppm *data;
 };
 
-/* data has to be an array of length 3 arries. width and height correspond to
- * the images width and height. depth is the maximum colour value, upperbounded
- * by 255.
+/* data has to be an array of length 3 arries. width and height correspond to the 
+ * images width and height. depth is the maximum colour value, upperbounded by 255
  */
-struct ppm *create_ppm(uint8_t *data[], unsigned int width, unsigned int height,
+struct ppm *create_ppm(uint8_t **data, unsigned int width, unsigned int height,
 		uint8_t depth);
 
 void destroy_ppm(struct ppm *image);
